3e041b83075f2dd9b4a7b98a93f5a5f17744d4d2
Joseph Alves <databentpx@gmail.com>
2015-06-15 13:07:34 -0400

    Delete

--- browser/js/common/factories/Reviews.js
+++
@@ -1,9 +0,0 @@
-app.factory('ReviewsFactory', function($http){
-	return{
-		getReviews: function (id) {
-			return $http.get('/api/review/'+id).then(function(review){
-				return review.data;
-			});
-		}
-	}
-});
